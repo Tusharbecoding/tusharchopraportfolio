ea56de9937c9fefacaf0815e637f0b8b865d67e3
Tusharbecoding <tusharchopra02122002@gmail.com>
2023-06-02 22:25:31 +0530

    added cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-
+import AnimatedCursor from "react-animated-cursor"
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 
@@ -19,6 +19,13 @@ const App = () => {
           <Contact />
           <StarsCanvas />
         </div>
+        <AnimatedCursor
+        innerSize={20}
+      outerSize={20}
+      color='255, 255, 255'
+      outerAlpha={0.2}
+      innerScale={0.7}
+      outerScale={5} />
       </div>
     </BrowserRouter>
   );
